439901b04cb955bf81b53f3fb461d32d5ae9cda7
milangupta95 <milangupta95@gmail.com>
2022-08-22 15:42:22 +0530

    Secured

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,51 +1,55 @@
 const mongoose = require('mongoose');
 const dblink = process.env.dblink || require('../../secrets').dblink;
-mongoose.
-connect(dblink,{useNewUrlParser: true, useUnifiedTopology: true}).
-then(function(){
-    console.log("SuccesFully Connected to Database");
-}).
-catch(function(err) {
-    console.log("error",err);
-});
-let userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true,"Name can't Be Empty"]
-    },
-    email: {
-        type: String,
-        required: [true,"Email can't be null"]
-    },
-    password: {
-        type: String,
-        required: [true,"Email or Password Missing"]
-    },
-    confirmPassword: {
-        type: String,
-        required: [true,"Confirm Password Can't be empty"],
+try {
+    mongoose.
+        connect(dblink, { useNewUrlParser: true, useUnifiedTopology: true }).
+        then(function () {
+            console.log("SuccesFully Connected to Database");
+        }).
+        catch(function (err) {
+            console.log("error", err);
+        });
+    let userSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: [true, "Name can't Be Empty"]
+        },
+        email: {
+            type: String,
+            required: [true, "Email can't be null"]
+        },
+        password: {
+            type: String,
+            required: [true, "Email or Password Missing"]
+        },
+        confirmPassword: {
+            type: String,
+            required: [true, "Confirm Password Can't be empty"],
 
-        validate : {
-            validator: function() {
-                return this.password == this.confirmPassword; 
-            },
-            message: "Password Miss Match"
+            validate: {
+                validator: function () {
+                    return this.password == this.confirmPassword;
+                },
+                message: "Password Miss Match"
+            }
+        },
+        phone: {
+            type: Number,
+            required: [true, "Phone Number can't be null"]
+        },
+        pic: {
+            type: String,
+            default: "default.jpg"
+        },
+        otp: {
+            type: Number
+        },
+        otpExpiry: {
+            type: Date
         }
-    },
-    phone: {
-        type: Number,
-        required: [true,"Phone Number can't be null"]
-    },
-    pic: {
-        type: String,
-        default: "default.jpg" 
-    },
-    otp: {
-        type: Number
-    },
-    otpExpiry: {
-        type: Date
-    } 
-});
-const userModel = mongoose.model('FoodUserModel',userSchema);
-module.exports = userModel;
+    });
+    const userModel = mongoose.model('FoodUserModel', userSchema);
+    module.exports = userModel;
+}catch(err) {
+    alert(err.message);
+}
